3dd29ccdf2b4b20ee5f9903d08656bdd465c4c09
thewoodagency <egnews02@gmail.com>
2023-08-04 23:47:24 -0500

    complete postcss setup

---
+++ app/src/App.js
@@ -0,0 +1,5 @@
+import '../assets/styles/styles.css';
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const postCSSPlugins = [
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer'),
+  require('postcss-import'),
+  require('postcss-mixins'),
+];
+
+module.exports = {
+  entry: './app/src/App.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'app'),
+  },
+  devServer: {
+    watchFiles: ['./app/**/*.html'],
+    static: {
+      directory: path.join(__dirname, 'app'),
+      watch: false,
+    },
+    hot: true,
+    port: 3000,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { url: false } },
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: postCSSPlugins } },
+          },
+        ],
+      },
+    ],
+  },
+};
