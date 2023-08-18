c028e2bd2cece12f9ecc22ea3a6a9493bf704fb2
thewoodagency <egnews02@gmail.com>
2023-08-18 15:14:19 -0500

    additional commit2

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
 import '../assets/styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import Scroll from './modules/Scroll';
+import StickHeader from './modules/StickyHeader';
 
-const mobileMenu = new MobileMenu();
+new MobileMenu();
 new Scroll(document.querySelectorAll('.feature-item'), 75);
 new Scroll(document.querySelectorAll('.testimonial'), 60);
+new StickHeader();
+let modal;
+
+document.querySelectorAll('.open-modal').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (typeof modal == 'undefined') {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then((x) => {
+        modal = new x.default();
+        setTimeout(() => modal.showModal(), 20);
+      });
+    } else {
+      modal.showModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
