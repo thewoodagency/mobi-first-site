ff60c71691209c44ec1bcb9cc8c72ff79d46e0f0
thewoodagency <egnews02@gmail.com>
2023-08-21 14:30:37 -0500

    update cloud fn

--- cloud-fn/member_check.js
+++ cloud-fn/member_check.js
@@ -1,6 +1,25 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'member checked',
-  });
+  const memberContent = `
+  <h1>Member checked</h1>
+  <p>Member has been checked</p>
+  `;
+
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.key === 'test') {
+    callback(null, {
+      statusCode: 200,
+      body: memberContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
